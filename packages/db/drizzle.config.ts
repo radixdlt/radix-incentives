b9f714aafa943322788760bea8463a084f386121
Alex Stelea <alex@stelea.io>
2025-04-07 13:21:20 +0100

    db: migrate db

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,6 +1,6 @@
-import { type Config } from "drizzle-kit";
+import type { Config } from "drizzle-kit";
 
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set')
+if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
 
 export default {
   schema: "./src/schema.ts",
@@ -10,5 +10,5 @@ export default {
   },
   strict: true,
   verbose: true,
-  tablesFilter: ["hookah_*"],
+  tablesFilter: [],
 } satisfies Config;
