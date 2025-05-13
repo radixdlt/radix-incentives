9821feb5d17eb0ac964f2d2587edc0a9b735ff28
Alex Stelea <alex.stelea@radixpublishing.com>
2025-05-13 12:19:01 +0100

    code: compare results between preview and octo lib

--- packages/api/src/common/dapps/caviarnine/getShapeLiquidityAssets.test.ts
+++ packages/api/src/common/dapps/caviarnine/getShapeLiquidityAssets.test.ts
@@ -11,12 +11,24 @@ import {
 } from "../../gateway/getNonFungibleBalance";
 import { GatewayApiClientLive } from "../../gateway/gatewayApiClient";
 import { createAppConfigLive } from "../../config/appConfig";
-import { GetStateVersionLive } from "../../gateway/getStateVersion";
+import {
+  GetStateVersionLive,
+  GetStateVersionService,
+} from "../../gateway/getStateVersion";
 import { GetEntityDetailsServiceLive } from "../../gateway/getEntityDetails";
 import { LoggerLive } from "../../logger/logger";
 import { EntityNonFungibleDataLive } from "../../gateway/entityNonFungiblesData";
 import { EntityNonFungiblesPageLive } from "../../gateway/entityNonFungiblesPage";
-import { shapeLiquidityReceiptSet } from "./constants";
+import { CaviarNineConstants, shapeLiquidityReceiptSet } from "./constants";
+import { getRedemptionValue } from "@stabilis/c9-shape-liquidity-getter";
+import { catchAll, tryPromise } from "effect/Effect";
+import {
+  GetResourceHoldersLive,
+  GetResourceHoldersService,
+} from "../../gateway/getResourceHolders";
+import path from "node:path";
+import { writeFile } from "node:fs/promises";
+import { BigNumber } from "bignumber.js";
 
 const coreApiClientLive = CoreApiClientLive;
 
@@ -65,62 +77,136 @@ const getNonfungibleBalanceLive = GetNonFungibleBalanceLive.pipe(
   Layer.provide(entityNonFungiblesPageLive)
 );
 
+const getResourceHoldersLive = GetResourceHoldersLive.pipe(
+  Layer.provide(gatewayApiClientLive)
+);
+
+const outputPath = path.join(__dirname, "output.json");
+
+const output: {
+  preview: {
+    xToken: BigNumber;
+    yToken: BigNumber;
+    nfId: string;
+  };
+  octoLib: {
+    xToken: BigNumber;
+    yToken: BigNumber;
+    nfId: string;
+  } | null;
+}[] = [];
+
 describe("getShapeLiquidityAssets", () => {
   it("should get the shape liquidity assets", async () => {
     const program = Effect.provide(
       Effect.gen(function* () {
         const getShapeLiquidityAssets = yield* GetShapeLiquidityAssetsService;
         const getNonfungibleBalance = yield* GetNonFungibleBalanceService;
+        const getStateVersionService = yield* GetStateVersionService;
+        const getResourceHoldersService = yield* GetResourceHoldersService;
 
-        const nonFungiblesResults = yield* getNonfungibleBalance({
-          addresses: [
-            "account_rdx12xwrtgmq68wqng0d69qx2j627ld2dnfufdklkex5fuuhc8eaeltq2k",
-          ],
+        const stateVersion = yield* getStateVersionService(new Date());
+
+        const resourceHolders = yield* getResourceHoldersService({
+          resourceAddress:
+            CaviarNineConstants.shapeLiquidityPools[2].liquidity_receipt,
         });
 
-        const [nonFungiblesResult] = nonFungiblesResults.items;
+        const addresses = resourceHolders.items.map(
+          (item) => item.holder_address
+        );
 
-        const shapeLiquidityNftCollections =
-          nonFungiblesResult.nonFungibleResources.filter((item) => {
-            return shapeLiquidityReceiptSet.has(item.resourceAddress);
-          });
+        const nonFungiblesResults = yield* getNonfungibleBalance({
+          addresses: addresses,
+          state: {
+            state_version: stateVersion.stateVersion,
+          },
+        });
 
-        for (const collection of shapeLiquidityNftCollections) {
-          for (const nft of collection.items) {
-            // biome-ignore lint/style/noNonNullAssertion: <explanation>
-            const shapeLiquidityPool = shapeLiquidityReceiptSet.get(
-              collection.resourceAddress
-            )!;
+        const nonFungiblesResultItems = nonFungiblesResults.items;
 
-            const { x, y } = yield* getShapeLiquidityAssets({
-              componentAddress: shapeLiquidityPool.componentAddress,
-              nonFungibleLocalId: nft.id,
-              networkId: 1,
+        for (const nonFungiblesResult of nonFungiblesResultItems) {
+          const shapeLiquidityNftCollections =
+            nonFungiblesResult.nonFungibleResources.filter((item) => {
+              return shapeLiquidityReceiptSet.has(item.resourceAddress);
             });
 
-            console.log(
-              JSON.stringify(
-                {
-                  shapeLiquidityPool,
-                  redemptionValue: {
-                    x: {
-                      resourceAddress: shapeLiquidityPool.token_x,
-                      value: x,
-                    },
-                    y: {
-                      resourceAddress: shapeLiquidityPool.token_y,
-                      value: y,
-                    },
-                  },
+          for (const collection of shapeLiquidityNftCollections) {
+            for (const nft of collection.items) {
+              // biome-ignore lint/style/noNonNullAssertion: <explanation>
+              const shapeLiquidityPool = shapeLiquidityReceiptSet.get(
+                collection.resourceAddress
+              )!;
+
+              const { x, y } = yield* getShapeLiquidityAssets({
+                componentAddress: shapeLiquidityPool.componentAddress,
+                nonFungibleLocalId: nft.id,
+                networkId: 1,
+                stateVersion: {
+                  state_version: stateVersion.stateVersion,
+                  type: "ByStateVersion",
                 },
-                null,
-                2
-              )
-            );
+              });
+
+              const octoLib = yield* tryPromise({
+                try: () =>
+                  getRedemptionValue({
+                    componentAddress: shapeLiquidityPool.componentAddress,
+                    nftId: nft.id,
+                    stateVersion: stateVersion.stateVersion,
+                  }),
+                catch: (error) => {
+                  console.error(
+                    "octoLib error",
+                    {
+                      componentAddress: shapeLiquidityPool.componentAddress,
+                      nftId: nft.id,
+                      stateVersion: stateVersion.stateVersion,
+                    },
+                    error
+                  );
+                  return error;
+                },
+              }).pipe(
+                catchAll((error) => {
+                  if (error instanceof Error) {
+                    if (error.message === "NFT not found") {
+                      return Effect.succeed(null);
+                    }
+                  }
+                  return Effect.fail(error);
+                })
+              );
+
+              console.log(
+                "preview",
+                JSON.stringify({ xToken: x, yToken: y, nfId: nft.id }, null, 2)
+              );
+
+              console.log(
+                "octoLib",
+                JSON.stringify({ ...octoLib, nfId: nft.id }, null, 2)
+              );
+
+              output.push({
+                preview: { xToken: x, yToken: y, nfId: nft.id },
+                octoLib: {
+                  xToken: octoLib?.xToken
+                    ? new BigNumber(octoLib.xToken)
+                    : new BigNumber(0),
+                  yToken: octoLib?.yToken
+                    ? new BigNumber(octoLib.yToken)
+                    : new BigNumber(0),
+                  nfId: nft.id,
+                },
+              });
+            }
           }
         }
 
-        return "";
+        writeFile(outputPath, JSON.stringify(output, null, 2));
+
+        return "done";
       }),
       Layer.mergeAll(
         getShapeLiquidityAssetsLive,
@@ -133,7 +219,8 @@ describe("getShapeLiquidityAssets", () => {
         entityNonFungibleDataLive,
         entityNonFungiblesPageLive,
         getStateVersionLive,
-        getEntityDetailsServiceLive
+        getEntityDetailsServiceLive,
+        getResourceHoldersLive
       )
     );
 
@@ -145,5 +232,5 @@ describe("getShapeLiquidityAssets", () => {
       console.error(JSON.stringify(error, null, 2));
       throw error;
     }
-  });
+  }, 300_000);
 });
