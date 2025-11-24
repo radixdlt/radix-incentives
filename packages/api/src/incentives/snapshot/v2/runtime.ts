b15239ea9e3b5dca77494c2672e1e47887da5a1c
Alex Stelea <alex.s@rdx.works>
2025-11-24 14:33:14 +0100

    annotate worker jobId to child logs

--- packages/api/src/incentives/snapshot/v2/runtime.ts
+++ packages/api/src/incentives/snapshot/v2/runtime.ts
@@ -1,4 +1,4 @@
-import { Layer, ManagedRuntime } from 'effect';
+import { Config, Effect, Layer, Logger, ManagedRuntime } from 'effect';
 import { CalculateActivityPointsWorkerService } from '../../activity-points/calculateActivityPointsWorker';
 import { EventWorkerService } from '../../events/eventWorker';
 import { LeaderboardCacheService } from '../../leaderboard/leaderboardCache';
@@ -8,8 +8,18 @@ import { CalculateSeasonPointsService } from '../../season-points/calculateSeaso
 import { WeekService } from '../../week/week';
 import { SnapshotV2Worker } from './snapshotV2Worker';
 
+const loggerLayer = Layer.unwrapEffect(
+  Effect.gen(function* () {
+    const prettyLogging = yield* Config.boolean('PRETTY_LOGGING').pipe(
+      Config.withDefault(false),
+    );
+    return prettyLogging ? Logger.pretty : Logger.json;
+  }),
+);
+
 export const workerRuntime = ManagedRuntime.make(
   Layer.mergeAll(
+    loggerLayer,
     SnapshotV2Worker.Default,
     SeasonPointsMultiplierWorkerService.Default,
     CalculateActivityPointsWorkerService.Default,
